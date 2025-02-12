e76aff427077310eae9346f713225e261d30c3e4
Kris9elsen <kristoffer9elsen@gmail.com>
2025-02-12 15:04:03 +0100

    First test of controling the robot

---
+++ test/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <ur_rtde/rtde_control_interface.h>
+#include <ur_rtde/rtde_receive_interface.h>
+#include <thread>
+#include <chrono>
+#include <string>
+
+using namespace ur_rtde;
+using namespace std::chrono;
+
+
+int main(int argc, char* argv[]){
+	RTDEControlInterface rtde_control("192.168.1.54");
+	RTDEReceiveInterface rtde_receive("192.168.1.54");
+
+	double velocity = 0.5;
+	double acceleration = 0.5;
+	double dt = 1.0/500;
+	double lookahead_time = 0.1;
+	double gain = 300;
+	std::vector<double> joint_q = {-0.98, -2.15, -0.36, 0.05, 3.14, 0.71};
+
+	rtde_control.moveJ(joint_q);
+
+	for (unsigned int i=0; i<1500; i++){
+		steady_clock::time_point t_start = rtde_control.initPeriod();
+		rtde_control.servoJ(joint_q, velocity, acceleration, dt, lookahead_time, gain);
+
+		std::vector<double> current_joint_positions = rtde_receive.getActualQ();
+
+		std::cout << "Current joint positions at step: " << i + 1 << std::endl;
+
+		for (const auto& joint : current_joint_positions){
+			std::cout << joint << std::endl;
+		}
+
+		joint_q[0] += 0.001;
+		joint_q[1] += 0.001;
+		rtde_control.waitPeriod(t_start);
+	}
+
+	rtde_control.servoStop();
+	rtde_control.stopScript();
+
+	return 0;
+}
