d24858927d480162d2bde95608da7a9257318df8
Kris9elsen <kristoffer9elsen@gmail.com>
2025-02-26 08:12:46 +0100

    Folder for robot control

---
+++ control/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	// Launch GUI, take user input
+	
+	// Function to calibrate robot
+	
+	// Main Loop going until finished.
+		// Get M&M Coordinates
+	
+		// Pick and place
+}
