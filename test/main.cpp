c08ea6a0f6a9312d116fe841671b1e4f016f2c22
Kris9elsen <kristoffer9elsen@gmail.com>
2025-02-27 17:55:21 +0100

    sim test

--- test/main.cpp
+++ test/main.cpp
@@ -11,9 +11,9 @@ using namespace std::chrono;
 
 int main(int argc, char* argv[]){
 	std::cout << "Connecting to control" << std::endl;
-	RTDEControlInterface rtde_control("192.168.1.54");
+	RTDEControlInterface rtde_control("192.168.0.133");
 	std::cout << "Connecting to recieve" << std::endl;
-	RTDEReceiveInterface rtde_receive("192.168.1.54");
+	RTDEReceiveInterface rtde_receive("192.168.0.133");
 	std::cout << "ALL Connections established!!" << std::endl;
 
 	double velocity = 0.5;
