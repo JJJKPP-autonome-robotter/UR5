902602c04ff9c5602074997100f31b39fa33c1d0
Kris9elsen <kristoffer9elsen@gmail.com>
2025-03-05 12:03:48 +0100

    Class constructor

--- control/headers/Gribber.hpp
+++ control/headers/Gribber.hpp
@@ -3,13 +3,25 @@
 
 #pragma once
 
+#include <boost/asio.hpp>
+#include <string>
+
+using namespace boost::asio;
+using namespace std;
+
 class Gribber
 {
 public:
     Gribber();
     ~Gribber();
+    Gribber(string, u_int);
 
 private:
+    io_service io;
+    serial_port port;
+
+    string port_name;
+    u_int baudrate;
 
 };
 
--- control/src/Gribber.cpp
+++ control/src/Gribber.cpp
@@ -1,11 +1,19 @@
 #include "../headers/Gribber.hpp"
 
-Gribber::Gribber()
-{
+Gribber::Gribber() {}
 
+Gribber::~Gribber() {
+    port.close();
 }
 
-Gribber::~Gribber()
-{
+Gribber::Gribber(string _port_name, u_int _baudrate) : port(io, _port_name) {
+    port_name = _port_name;
+    baudrate = _baudrate;
 
+    // Set options for the serial port
+    port.set_option(serial_port_base::baud_rate(baudrate));
+    port.set_option(serial_port_base::character_size(8));
+    port.set_option(serial_port_base::parity(serial_port_base::parity::none));
+    port.set_option(serial_port_base::stop_bits(serial_port_base::stop_bits::one));
+    port.set_option(serial_port_base::flow_control(serial_port_base::flow_control::none));
 }
